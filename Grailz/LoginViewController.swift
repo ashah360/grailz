c3fb6686e235fbf069584343b9b181dcafcc084d
Ally <allier0416@gmail.com>
2018-12-10 01:57:58 -0800

    done account and login

--- Grailz/LoginViewController.swift
+++ Grailz/LoginViewController.swift
@@ -10,21 +10,65 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var emailTF: UITextField!
+    @IBOutlet weak var pwdTF: UITextField!
+    
+    
+    var user : String? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func loginTouchUpInside(_ sender: Any) {
+        let email = emailTF.text
+        let pwd = pwdTF.text
+        if !((email?.isValidEmail())!) {
+            let alert = UIAlertController(title: nil, message: "Invaild Email Adress", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        } else {
+            var request = URLRequest(url: URL(string: "https://graliz-account.herokuapp.com/login")!)
+            request.httpMethod = "POST"
+            request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+            let postString = "email=\(email!)&password=\(pwd!)"
+            request.httpBody = postString.data(using: .utf8)
+            URLSession.shared.dataTask(with: request) { (data, response, err) in
+                guard let data = data, err == nil else {
+                    return
+                }
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as! [String: Any]
+                    if let result = json["result"] as? String {
+                        if result == "success" {
+                            self.user = (json["username"] as! String)
+                            DispatchQueue.main.async {
+                                self.performSegue(withIdentifier: "toAccount", sender: self)
+                            }
+                        }
+                    }
+                } catch let jsonErr {
+                    print("Error serialize json: ", jsonErr)
+                }
+            }.resume()
+        }
     }
-    */
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toAccount" {
+            let accountVC = segue.destination as! AccountViewController
+            accountVC.user = user
+        }
+    }
+    
+}
 
+
+extension String {
+    func isValidEmail() -> Bool {
+        let regex = try! NSRegularExpression(pattern: "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$", options: .caseInsensitive)
+        return regex.firstMatch(in: self, options: [], range: NSRange(location: 0, length: count)) != nil
+    }
 }
