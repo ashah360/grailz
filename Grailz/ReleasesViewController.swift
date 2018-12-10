54252796e0b2a46cf3613228e52d161e85a90780
Sanghawk <shlee95@uw.edu>
2018-12-10 14:58:55 -0800

    Locking orientation for AccountVC and ShoeDetailVC

--- Grailz/ReleasesViewController.swift
+++ Grailz/ReleasesViewController.swift
@@ -9,124 +9,140 @@
 import UIKit
 
 struct productList: Decodable {
-    let created_at: String?
-    let _id: String?
-    let title: String?
-    let active: Bool?
-    let release: String?
-    let images: [String: String]?
-    let price: [String: String]?
-    let votes: [[String: String]]?
-    let _v: Int?
+  let created_at: String?
+  let _id: String?
+  let title: String?
+  let active: Bool?
+  let release: String?
+  let images: [String: String]?
+  let price: [String: String]?
+  let votes: [[String: String]]?
+  let _v: Int?
 }
 
 class ReleasesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+  
+  @IBOutlet weak var tblReleases: UITableView!
+  
+  // For locking orientation to portrait
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    AppDelegate.AppUtility.lockOrientation(.portrait)
+  }
+  
+  // For locking orientation to portrait
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+    AppDelegate.AppUtility.lockOrientation(.all)
+  }
+  
+  // For locking orientation to portrait
+  override open var shouldAutorotate: Bool {
+    return false
+  }
+  
+  var appData = ShoesData.shared
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return self.appData.releaseList.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tblReleases.dequeueReusableCell(withIdentifier: "Shoe") as! ShoeCellTableViewCell
+    cell.imgPic.image = UIImage(data: self.appData.releaseList[indexPath.row].img)
+    cell.lblTitle.text = self.appData.releaseList[indexPath.row].title
+    cell.lblRelease.text = self.appData.releaseList[indexPath.row].release
     
-    @IBOutlet weak var tblReleases: UITableView!
+    date()
     
-    
-    var appData = ShoesData.shared
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.appData.releaseList.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tblReleases.dequeueReusableCell(withIdentifier: "Shoe") as! ShoeCellTableViewCell
-        cell.imgPic.image = UIImage(data: self.appData.releaseList[indexPath.row].img)
-        cell.lblTitle.text = self.appData.releaseList[indexPath.row].title
-        cell.lblRelease.text = self.appData.releaseList[indexPath.row].release
-        
-        date()
-        
-        return cell
-    }
-    
-    func date() {
-        let date = self.appData.releaseList[appData.row].release
-        var month = date.firstIndex(of: "-")
-//        switch month {
-//        case month = "01-":
-//            month = "January"
-//        case month = "02-":
-//            month = "February"
-//        case month = "03-":
-//            month = "March"
-//        case month = "04-":
-//            month = "April"
-//        case month = "05-":
-//            month = "May"
-//        case month == "06-":
-//            month = "June"
-//        case month == "07-":
-//            month = "July"
-//        case month == "08-":
-//            month = "August"
-//        case month == "09-":
-//            month = "September"
-//        case month == "10-":
-//            month = "October"
-//        case month == "11-":
-//            month = "November"
-//        case month == "12-":
-//            month = "December"
-//        default:
-//            month = "error"
-//        }
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 330
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        self.appData.row = indexPath.row
-        performSegue(withIdentifier: "toShoeDetails", sender: self)
-    }
-    
-    func fetchJSON() {
-        let base = "http://grailz.herokuapp.com/api/"
-        guard let url =  URL(string: "\(base)product/list") else { return }
-        URLSession.shared.dataTask(with: url) { (data, response, err) in DispatchQueue.main.async {
-            guard let data = data else { return }
+    return cell
+  }
+  
+  func date() {
+    let date = self.appData.releaseList[appData.row].release
+    var month = date.firstIndex(of: "-")
+    //        switch month {
+    //        case month = "01-":
+    //            month = "January"
+    //        case month = "02-":
+    //            month = "February"
+    //        case month = "03-":
+    //            month = "March"
+    //        case month = "04-":
+    //            month = "April"
+    //        case month = "05-":
+    //            month = "May"
+    //        case month == "06-":
+    //            month = "June"
+    //        case month == "07-":
+    //            month = "July"
+    //        case month == "08-":
+    //            month = "August"
+    //        case month == "09-":
+    //            month = "September"
+    //        case month == "10-":
+    //            month = "October"
+    //        case month == "11-":
+    //            month = "November"
+    //        case month == "12-":
+    //            month = "December"
+    //        default:
+    //            month = "error"
+    //        }
+  }
+  
+  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    return 330
+  }
+  
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    self.appData.row = indexPath.row
+    performSegue(withIdentifier: "toShoeDetails", sender: self)
+  }
+  
+  func fetchJSON() {
+    let base = "http://grailz.herokuapp.com/api/"
+    guard let url =  URL(string: "\(base)product/list") else { return }
+    URLSession.shared.dataTask(with: url) { (data, response, err) in DispatchQueue.main.async {
+      guard let data = data else { return }
+      do {
+        let stringData = String(data: data, encoding: .utf8)
+        print(stringData)
+        let shoes =  try JSONDecoder().decode([productList].self, from: data)
+        var index = 0
+        for shoe in shoes {
+          
+          let images = shoe.images
+          var data: Data? = nil
+          
+          if let imgUrl = URL(string: (images?["product"])!) {
             do {
-                let stringData = String(data: data, encoding: .utf8)
-                print(stringData)
-                let shoes =  try JSONDecoder().decode([productList].self, from: data)
-                var index = 0
-                for shoe in shoes {
-                    
-                    let images = shoe.images
-                    var data: Data? = nil
-                    
-                    if let imgUrl = URL(string: (images?["product"])!) {
-                        do {
-                            data = try Data(contentsOf: imgUrl)
-                        } catch let err {
-                            print("Error loading picture: \(err)")
-                        }
-                    }
-                    
-                    let x = shoeElement(index: index, title: shoe.title!, release: shoe.release!, img: data!, _id: shoe._id!, price: shoe.price!, votes: shoe.votes!)
-                    self.appData.releaseList.append(x)
-                    index += 1
-                }
-                self.tblReleases.reloadData()
-            } catch {
-                print("Error")
+              data = try Data(contentsOf: imgUrl)
+            } catch let err {
+              print("Error loading picture: \(err)")
             }
-            }
-        }.resume()
-    }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.appData.row = 0
-        self.appData.releaseList = []
-        fetchJSON()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-
+          }
+          
+          let x = shoeElement(index: index, title: shoe.title!, release: shoe.release!, img: data!, _id: shoe._id!, price: shoe.price!, votes: shoe.votes!)
+          self.appData.releaseList.append(x)
+          index += 1
+        }
+        self.tblReleases.reloadData()
+      } catch {
+        print("Error")
+      }
+      }
+      }.resume()
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.appData.row = 0
+    self.appData.releaseList = []
+    fetchJSON()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+  
+  
 }
 
--- Grailz/ShoeDetailsViewController.swift
+++ Grailz/ShoeDetailsViewController.swift
@@ -16,201 +16,218 @@ class YoutubeCell: UITableViewCell {
 }
 
 struct YouTubeVideo {
-    var title: String
-    var desc: String
-    var img: String
-    var url: String
+  var title: String
+  var desc: String
+  var img: String
+  var url: String
+  
+  init(json: [String: Any]) {
+    let id = json["id"] as! [String: Any]
+    let snippet = json["snippet"] as! [String: Any]
     
-    init(json: [String: Any]) {
-        let id = json["id"] as! [String: Any]
-        let snippet = json["snippet"] as! [String: Any]
-        
-        url = "https://www.youtube.com/watch?v=" + (id["videoId"] as! String)
-        title = snippet["title"] as! String
-        desc = snippet["description"] as! String
-        let thumb = snippet["thumbnails"] as! [String: Any]
-        let defaultThumb = thumb["default"] as! [String: Any]
-        img = defaultThumb["url"] as! String
-
-    }
+    url = "https://www.youtube.com/watch?v=" + (id["videoId"] as! String)
+    title = snippet["title"] as! String
+    desc = snippet["description"] as! String
+    let thumb = snippet["thumbnails"] as! [String: Any]
+    let defaultThumb = thumb["default"] as! [String: Any]
+    img = defaultThumb["url"] as! String
+    
+  }
 }
 
 class ShoeDetailsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+  
+  @IBOutlet weak var imgPic: UIImageView!
+  @IBOutlet weak var lblTitle: UILabel!
+  @IBOutlet weak var lblRelease: UILabel!
+  @IBOutlet weak var btnBack: UIButton!
+  @IBOutlet weak var tblVideoReviews: UITableView!
+  @IBOutlet weak var btnCop: UIButton!
+  @IBOutlet weak var btnPass: UIButton!
+  
+  
+  var youTubeVideos : [YouTubeVideo] = []
+  
+  // For locking orientation to portrait
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    AppDelegate.AppUtility.lockOrientation(.portrait)
+  }
+  
+  // For locking orientation to portrait
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+    AppDelegate.AppUtility.lockOrientation(.all)
+  }
+  
+  // For locking orientation to portrait
+  override open var shouldAutorotate: Bool {
+    return false
+  }
+  
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    if let url = URL(string: youTubeVideos[indexPath.row].url),
+      UIApplication.shared.canOpenURL(url) {
+      UIApplication.shared.open(url, options: [:], completionHandler: nil)
+    }
     
-    @IBOutlet weak var imgPic: UIImageView!
-    @IBOutlet weak var lblTitle: UILabel!
-    @IBOutlet weak var lblRelease: UILabel!
-    @IBOutlet weak var btnBack: UIButton!
-    @IBOutlet weak var tblVideoReviews: UITableView!
-    @IBOutlet weak var btnCop: UIButton!
-    @IBOutlet weak var btnPass: UIButton!
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return youTubeVideos.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "youTubeCell", for: indexPath) as! YoutubeCell
+    let video = youTubeVideos[indexPath.row]
+    cell.title?.text = video.title
+    cell.desc?.text = video.desc
+    if let imgUrl = URL(string: video.img) {
+      do {
+        let data = try Data(contentsOf: imgUrl)
+        cell.img.image = UIImage(data: data)
+      } catch let err {
+        print(err)
+      }
+    }
+    return cell
+  }
+  
+  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    return 90
+  }
+  
+  var appData = ShoesData.shared
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
+    buttonShadow(button: btnCop)
+    buttonShadow(button: btnPass)
     
-    var youTubeVideos : [YouTubeVideo] = []
+    imgPic.image = UIImage(data: appData.releaseList[appData.row].img)
+    lblTitle.text = appData.releaseList[appData.row].title
+    lblRelease.text = appData.releaseList[appData.row].release
     
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if let url = URL(string: youTubeVideos[indexPath.row].url),
-            UIApplication.shared.canOpenURL(url) {
-            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+    self.tblVideoReviews.dataSource = self
+    self.tblVideoReviews.delegate = self
+    self.tblVideoReviews.tableFooterView = UIView()
+    loadURL()
+  }
+  
+  func buttonShadow(button: UIButton) {
+    button.layer.shadowColor = UIColor.black.cgColor
+    button.layer.shadowOffset = CGSize(width: 0, height: 5)
+    button.layer.shadowRadius = 5
+    button.layer.shadowOpacity = 0.5
+  }
+  
+  func loadURL() {
+    let keyword = "\(appData.releaseList[appData.row].title) review"
+    // TODO use an actual keyword after sneaker data is loaded
+    let apiKey = "AIzaSyDpvNb3Ro5qwtCbZAekxgbMzbPvSrEV4so"
+    let urlString = "https://www.googleapis.com/youtube/v3/search"
+    var youtubeURL = URLComponents(string: urlString)
+    youtubeURL?.queryItems = [
+      URLQueryItem(name: "part", value: "snippet"),
+      URLQueryItem(name: "maxResults", value: "2"),
+      URLQueryItem(name: "q", value: keyword),
+      URLQueryItem(name: "type", value: "video"),
+      URLQueryItem(name: "key", value: apiKey)
+    ]
+    URLSession.shared.dataTask(with: (youtubeURL?.url)!) { (data, response, err) in
+      if let error = err {
+        print("URL Session Error: ", error)
+      }
+      
+      if let data = data {
+        do {
+          let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as! [String: Any]
+          if let videoArray = json["items"] as? [Any] {
+            for video in videoArray {
+              self.youTubeVideos.append(YouTubeVideo(json: video as! [String : Any]))
+            }
+            DispatchQueue.main.async {
+              self.tblVideoReviews.reloadData()
+            }
+          }
+        } catch let jsonErr {
+          print("Error serialize json: ", jsonErr)
         }
-
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return youTubeVideos.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "youTubeCell", for: indexPath) as! YoutubeCell
-        let video = youTubeVideos[indexPath.row]
-        cell.title?.text = video.title
-        cell.desc?.text = video.desc
-        if let imgUrl = URL(string: video.img) {
-            do {
-                let data = try Data(contentsOf: imgUrl)
-                cell.img.image = UIImage(data: data)
-            } catch let err {
-                print(err)
-            }
+      }
+      }.resume()
+  }
+  
+  @IBAction func btnCop(_ sender: Any) {
+    print("Cop")
+    getVotes()
+    postSetup()
+    getVotes()
+  }
+  
+  @IBAction func btnPass(_ sender: Any) {
+    print("Pass")
+  }
+  
+  func getVotes() {
+    guard let url = URL(string: "http://grailz.herokuapp.com/api/product/\(appData.releaseList[appData.row]._id)") else { return }
+    let session = URLSession.shared
+    session.dataTask(with: url) { (data, response, err) in
+      if let response = response {
+        print(response)
+      }
+      if let data = data {
+        do {
+          let json = try JSONSerialization.jsonObject(with: data, options: [])
+          print(json)
+        } catch {
+          print(error)
         }
-        return cell
-    }
+      }
+      }.resume()
+  }
+  
+  public func postSetup() {
+    let parameters = ["result": 0]
     
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 90
-    }
+    guard let url = URL(string: "http://grailz.herokuapp.com/api/product/\(appData.releaseList[appData.row]._id)/vote") else {return}
     
-    var appData = ShoesData.shared
+    print(url)
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        buttonShadow(button: btnCop)
-        buttonShadow(button: btnPass)
-        
-        imgPic.image = UIImage(data: appData.releaseList[appData.row].img)
-        lblTitle.text = appData.releaseList[appData.row].title
-        lblRelease.text = appData.releaseList[appData.row].release
-        
-        self.tblVideoReviews.dataSource = self
-        self.tblVideoReviews.delegate = self
-        self.tblVideoReviews.tableFooterView = UIView()
-        loadURL()
-    }
-    
-    func buttonShadow(button: UIButton) {
-        button.layer.shadowColor = UIColor.black.cgColor
-        button.layer.shadowOffset = CGSize(width: 0, height: 5)
-        button.layer.shadowRadius = 5
-        button.layer.shadowOpacity = 0.5
-    }
-    
-    func loadURL() {
-        let keyword = "\(appData.releaseList[appData.row].title) review"
-        // TODO use an actual keyword after sneaker data is loaded
-        let apiKey = "AIzaSyDpvNb3Ro5qwtCbZAekxgbMzbPvSrEV4so"
-        let urlString = "https://www.googleapis.com/youtube/v3/search"
-        var youtubeURL = URLComponents(string: urlString)
-        youtubeURL?.queryItems = [
-            URLQueryItem(name: "part", value: "snippet"),
-            URLQueryItem(name: "maxResults", value: "2"),
-            URLQueryItem(name: "q", value: keyword),
-            URLQueryItem(name: "type", value: "video"),
-            URLQueryItem(name: "key", value: apiKey)
-        ]
-        URLSession.shared.dataTask(with: (youtubeURL?.url)!) { (data, response, err) in
-            if let error = err {
-                print("URL Session Error: ", error)
-            }
-            
-            if let data = data {
-                do {
-                    let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as! [String: Any]
-                    if let videoArray = json["items"] as? [Any] {
-                        for video in videoArray {
-                            self.youTubeVideos.append(YouTubeVideo(json: video as! [String : Any]))
-                        }
-                        DispatchQueue.main.async {
-                            self.tblVideoReviews.reloadData()
-                        }
-                    }
-                } catch let jsonErr {
-                    print("Error serialize json: ", jsonErr)
-                }
-            }
-        }.resume()
-    }
-    
-    @IBAction func btnCop(_ sender: Any) {
-        print("Cop")
-        getVotes()
-        postSetup()
-        getVotes()
-    }
-    
-    @IBAction func btnPass(_ sender: Any) {
-        print("Pass")
-    }
-    
-    func getVotes() {
-        guard let url = URL(string: "http://grailz.herokuapp.com/api/product/\(appData.releaseList[appData.row]._id)") else { return }
-        let session = URLSession.shared
-        session.dataTask(with: url) { (data, response, err) in
-            if let response = response {
-                print(response)
-            }
-            if let data = data {
-                do {
-                    let json = try JSONSerialization.jsonObject(with: data, options: [])
-                    print(json)
-                } catch {
-                    print(error)
-                }
-            }
-        }.resume()
-    }
-
-    public func postSetup() {
-        let parameters = ["result": 0]
-        
-        guard let url = URL(string: "http://grailz.herokuapp.com/api/product/\(appData.releaseList[appData.row]._id)/vote") else {return}
-        
-            print(url)
-        
-            var request = URLRequest(url: url)
-            request.httpMethod = "POST"
-            guard let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: []) else {return}
-            request.httpBody = httpBody
-            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-            let session = URLSession.shared
-            session.dataTask(with: request) { (data, response, err) in
-                if let response = response {
-                    print(response)
-                }
-                if let data = data {
-                    do {
-                        let json = try JSONSerialization.jsonObject(with: data, options: [])
-                    } catch {
-                        print(error)
-                    }
-                }
-            }.resume()
-    }
-    
-    public func postVote(vote: Int) {
-        //        print("http://grailz.herokuapp.com/api/product/\(appData.releaseList[appData.row]._id)")
-        //
-        //        let parameters = ["votes": appData.releaseList[appData.row]]
-        //
-        //        guard let url = URL(string: "http://grailz.herokuapp.com/api/product/\(appData.releaseList[appData.row]._id)") else {return}
-        //        var request = URLRequest(url: url)
-        //        request.httpMethod = "POST"
-        //        guard let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: []) else {return}
-        //        request.httpBody = httpBody
-        //        let session = URLSession.shared
-        //        session.dataTask(with: request) { (data, response, err) in
-        //            <#code#>
-        //        }
-    }
-    
+    var request = URLRequest(url: url)
+    request.httpMethod = "POST"
+    guard let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: []) else {return}
+    request.httpBody = httpBody
+    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+    let session = URLSession.shared
+    session.dataTask(with: request) { (data, response, err) in
+      if let response = response {
+        print(response)
+      }
+      if let data = data {
+        do {
+          let json = try JSONSerialization.jsonObject(with: data, options: [])
+        } catch {
+          print(error)
+        }
+      }
+      }.resume()
+  }
+  
+  public func postVote(vote: Int) {
+    //        print("http://grailz.herokuapp.com/api/product/\(appData.releaseList[appData.row]._id)")
+    //
+    //        let parameters = ["votes": appData.releaseList[appData.row]]
+    //
+    //        guard let url = URL(string: "http://grailz.herokuapp.com/api/product/\(appData.releaseList[appData.row]._id)") else {return}
+    //        var request = URLRequest(url: url)
+    //        request.httpMethod = "POST"
+    //        guard let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: []) else {return}
+    //        request.httpBody = httpBody
+    //        let session = URLSession.shared
+    //        session.dataTask(with: request) { (data, response, err) in
+    //            <#code#>
+    //        }
+  }
+  
 }
